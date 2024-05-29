e2a4d390f6faf3c3fca05c9d202f2c1de0a008b2
JuanLetamendia22 <juanletamendia22@gmail.com>
2024-05-29 01:08:25 -0300

    Changes in the excercises, i made them functions

--- ejerciciosCondicional.php
+++ ejerciciosCondicional.php
@@ -1,40 +1,67 @@
 <?php
 echo 'Ejercicio 1<br>';
-$puntajeExamen=90;
-$calificacion=' ';
-if($puntajeExamen>=90){
-    $califcacion='A';
-    echo 'Su calificacion es:'.$calificacion;
-}elseif($puntajeExamen<90 && $puntajeExamen>=80){
-        echo 'Su calificacion es: B';
-}elseif($puntajeExamen<80 && $puntajeExamen>=70){
-    $califcacion='C';
-    echo 'Su calificacion es: C';
-}else{
-    $califcacion='F';
-    echo 'Su calificacion es: F';
+function calificarExamen($puntajeExamen){
+    if(is_int($puntajeExamen)){
+        $calificacion=" ";
+        if($puntajeExamen>=90){
+            $calificacion="A";
+            echo 'Su calificacion es: '. $calificacion;
+        }elseif($puntajeExamen<90 && $puntajeExamen>=80){
+            $calificacion ="B";
+            echo 'Su calificacion es: '. $calificacion;
+        }elseif($puntajeExamen<80 && $puntajeExamen>=70){
+            $calificacion="C";
+            echo 'Su calificacion es: '. $calificacion;
+        }else{
+            $calificacion="F";
+            echo 'Su calificacion es: '. $calificacion;
+        }
+    }else{
+        echo 'No ha introducido un numero, o puede ser que sí pero como un string';
+    }
+        
 }
+
+calificarExamen(90);
 
 
 echo '<br>Ejercicio 2<br>';
-$hora=3;
-if($hora>=6 && $hora<=12){
-    echo 'Buenos dias';
-}elseif($hora>12 && $hora<18){
-    echo 'Buenas tardes';
-}else{
-    echo 'Buenas noches';
+
+function saludarSegunHora($hora){
+    if(is_int($hora)){
+        if($hora>=6 && $hora<=12){
+            echo 'Buenos dias';
+        }elseif($hora>12 && $hora<18){
+            echo 'Buenas tardes';
+        }elseif($hora>24){
+            echo 'No introdujo una hora correcta, recuerde solo de 0-24hrs';
+        }else{
+            echo 'Buenas noches';
+        }
+    }else{
+        echo 'No ha introducido un numero, o puede ser que sí pero como un string';
+    }
 }
+
+saludarSegunHora(6);
+
 
 echo '<br>Ejercicio 3<br>';
-
-$edadPersona=68;
-if($edadPersona<18){
-    echo 'Es menor de edad';
-}elseif($edadPersona>18&& $edadPersona<=64){
-    echo 'Eres un adulto';
-}else{
-    echo 'Es una persona mayor';
+function clasificacarSegunEdad($edadPersona){
+    if(is_int($edadPersona)){
+        if($edadPersona<18){
+            echo 'Es menor de edad';
+        }elseif($edadPersona>18&& $edadPersona<=64){
+            echo 'Eres un adulto';
+        }else{
+            echo 'Es una persona mayor';
+        }    
+    }else{
+        echo 'No ha introducido un numero, o puede ser que sí pero como un string';
+    }
+  
 }
+
+clasificacarSegunEdad(30);
 
 ?>
